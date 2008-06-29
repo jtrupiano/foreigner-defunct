e70ced2169b5a6913c406b7e7422b424e249d334
John Trupiano <john@john.flicker.info>
2008-06-29 13:58:48 -0400

    Ignore polymorphic belongs_to associations.

--- lib/foreigner/verifier.rb
+++ lib/foreigner/verifier.rb
@@ -32,19 +32,24 @@ class Verifier
       table_name = model.table_name
       #puts "looking for #{table_name}"
 
-      model.reflections.each do |key, value|
-        case value.macro
+      # assocation is the name used to access the association, 
+      # reflection is the actual A/R object that represents the association
+      model.reflections.each do |association, reflection|
+        case reflection.macro
         when :belongs_to then
-          #puts "   belongs_to : #{key}"
-          primary_class_table = value.class_name.tableize
-          foreign_key_field = value.respond_to?(:foreign_key) ? value.foreign_key : value.name.to_s + '_id'
+          # ignore polymorphics
+          next if reflection.options[:polymorphic]
+
+          #puts "   belongs_to : #{association}"
+          primary_class_table = reflection.class_name.tableize
+          foreign_key_field = reflection.respond_to?(:foreign_key) ? reflection.foreign_key : reflection.name.to_s + '_id'
 
           tmp_fk = ForeignKey.new(table_name, foreign_key_field, primary_class_table, "id")
         when :has_and_belongs_to_many then
-          pk_table = value.class_name.tableize
+          pk_table = reflection.class_name.tableize
           pk_field = "id"
-          fk_table = value.options[:join_table] # the join table
-          fk_field = value.class_name.foreign_key
+          fk_table = reflection.options[:join_table] # the join table
+          fk_field = reflection.class_name.foreign_key
           
           tmp_fk = ForeignKey.new(fk_table, fk_field, pk_table, pk_field)
         else
@@ -53,7 +58,7 @@ class Verifier
         
         # check to see if our database has the foriegn key
         unless fks.reject! {|fk| fk == tmp_fk}
-          missing_fks[value.macro] << tmp_fk
+          missing_fks[reflection.macro] << tmp_fk
         end
       end
       
