6831dd3d6b7c10cc1f18a877d1667fe1d7c8dac3
John Trupiano <john@john.flicker.info>
2008-06-29 13:17:37 -0400

    Reorganized the ForeignKeys.retrieve method, making it simpler to add support for other db adapters.  Still only supporting mysql.

--- lib/foreigner/foreign_keys.rb
+++ lib/foreigner/foreign_keys.rb
@@ -3,15 +3,20 @@ require 'foreigner/foreign_key'
 class ForeignKeys
   
   def self.retrieve(configuration)
-    result = ActiveRecord::Base.connection.execute(sql(configuration['database']))
-    ret = []
-    result.each_hash do |row|
-      ret << ForeignKey.new(row['table_name'], row['column_name'], row['referenced_table_name'], row['referenced_column_name'])
+    fks = []
+    case configuration['adapter']
+    when 'mysql':
+      result = ActiveRecord::Base.connection.execute(mysql_sql(configuration['database']))
+      result.each_hash do |row|
+        fks << ForeignKey.new(row['table_name'], row['column_name'], row['referenced_table_name'], row['referenced_column_name'])
+      end
+    else
+      raise StandardError, "#{configuration['adapter']} is not currently supported"
     end
-    ret
+    fks
   end
 
-  def self.sql(schema_name)
+  def self.mysql_sql(schema_name)
     sql = <<-END_OF_SQL
       SELECT table_name, column_name, referenced_table_name, referenced_column_name
       FROM information_schema.KEY_COLUMN_USAGE
